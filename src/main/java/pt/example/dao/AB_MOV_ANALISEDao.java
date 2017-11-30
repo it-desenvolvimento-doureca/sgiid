b093cd7edbabc945b2fc57a1e3cc014683fe38ff
it-desenvolvimento-doureca <it2@doureca.local>
2017-11-30 17:07:32 +0000

    comiit

--- src/main/java/pt/example/dao/AB_MOV_ANALISEDao.java
+++ src/main/java/pt/example/dao/AB_MOV_ANALISEDao.java
@@ -43,7 +43,8 @@ public class AB_MOV_ANALISEDao extends GenericDaoJpaImpl<AB_MOV_ANALISE, Integer
 	public List<AB_MOV_ANALISE> getallmanu(Integer linha, Integer idbanho, Date datas) {
 		Query query = entityManager
 				.createQuery("Select a,b,c,d from AB_MOV_ANALISE a,AB_DIC_LINHA b,AB_DIC_BANHO c,AB_DIC_TINA d where  "
-						+ "a.ID_BANHO = :idbanho and a.ID_ANALISE not in(select e.ID_ANALISE from AB_MOV_MANUTENCAO_CAB e where e.ID_ANALISE is not null) and a.ESTADO in ('C','V') and  "
+						//+ "a.ID_BANHO = :idbanho and a.ID_ANALISE not in(select e.ID_ANALISE from AB_MOV_MANUTENCAO_CAB e where e.ID_ANALISE is not null) and a.ESTADO in ('C','V') and  "
+						+ "a.ID_BANHO = :idbanho and a.ESTADO in ('C','V') and  "
 						+ "a.ID_LINHA = b.ID_LINHA and a.ID_BANHO = c.ID_BANHO and c.ID_TINA = d.ID_TINA and a.INATIVO not in (1) "
 						+ "and ((not :linha != 0) or (a.ID_LINHA = :linha))  and a.DATA_ANALISE > :datas  order by a.ID_ANALISE desc");
 		query.setParameter("linha", linha);
--- src/main/webapp/assets/config/webUrl.ts
+++ src/main/webapp/assets/config/webUrl.ts
@@ -2,10 +2,10 @@ export var webUrl = {
 
   host: 'http://192.168.40.101:8080/sgiid',
   mod_pro: true
+
+  /*host: 'http://localhost:8080/sgiid',
+  mod_pro: false,*/
   /*
-    host: 'http://localhost:8080/sgiid',
     mod_pro: false,
-    
-      mod_pro: false,
-      host: 'http://192.168.40.101:8080/sgiid_dev'*/
+    host: 'http://192.168.40.101:8080/sgiid_dev'*/
 }
